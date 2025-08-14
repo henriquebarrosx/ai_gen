65d436879ec1eb0fac52e823b2ff64cf6426b4a8
Henrique Barros <henriquebarrosx@gmail.com>
2025-08-14 14:38:54 -0300

    modifica formato da exibição da data e hora nas mensagens de logs

--- adapter/logger/logger.ts
+++ adapter/logger/logger.ts
@@ -1,12 +1,12 @@
 export class Logger {
 	info(message: string, ...args: unknown[]): void {
-		const datetime = new Date().toISOString();
+		const datetime = new Date().toString();
 		const msg = `[${datetime}] INFO: ${message}`
 		console.log(msg, ...args);
 	}
 
 	error(message: string, ...args: unknown[]): void {
-		const datetime = new Date().toISOString();
+		const datetime = new Date().toString();
 		const msg = `[${datetime}] ERROR: ${message}`
 		console.log(msg, ...args);
 	}
